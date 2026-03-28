02400a15d2bc314a545f5a7ea5f4e70e77c7e206
Abu1123 <Tounkes88@gmail.com>
2026-03-28 11:50:16 -0400

    Add files via upload

---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Art Gallery System</title>
+</head>
+<body>
+    
+<header>
+    <h2>Art Gallery System</h2>
+
+    <a href="index.php">Home</a> 
+     <li><a href="artworks.php">View Artworks</a></li>
+    <li><a href="exhibitions.php">View Exhibitions</a></li>
+    <li><a href="sales.php">View Sales</a></li>
+
+    <hr>
+</header>
+
+</body>
+</html>
